8e5e39fc2a6ef86992c8808b2e4bd7863e71f6c7
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-09-17 11:35:37 +0100

    capitalise model name

--- app/Models/personalLog.php
+++ app/Models/personalLog.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class personalLog extends Model
+class PersonalLog extends Model
 {
     use HasFactory;
 }
