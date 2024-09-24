794a9ae68dd459da66fe9661311d73efb6129607
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-09-24 15:49:41 +0100

    create base styling, add to base template

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>@yield('title', '75Hard tracker')</title>
     <meta charset="UTF-8">
+    <meta name="description" content="@yield('meta_description', 'Welcome to 75Hard fitness and diet tracker - this site can be used to log progress throughout your journey.')">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="robots" content="@yield('robots_meta', 'index, follow')">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @yield('scripts')
     <title>Document</title>
 </head>
 <body>
