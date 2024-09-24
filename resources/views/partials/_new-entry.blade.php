07c566205e4186c3fe3a04ff3e3b7d1b12879f43
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-09-24 22:14:40 +0100

    make blade logic more efficient

--- resources/views/partials/_new-entry.blade.php
+++ resources/views/partials/_new-entry.blade.php
@@ -33,7 +33,7 @@ $max_date = $new_past_entry ? date('Y-m-d', strtotime('-1 day')) : date("Y-m-d")
         <div class="field field--workout-notes">
             <label for="workout_notes">Workout notes</label>
 
-            <textarea name="workout_notes" id="workout_notes" rows="3" cols="50">{{ $existing_entry? $existing_entry['workout_notes'] : '' }}</textarea>
+            <textarea name="workout_notes" id="workout_notes" rows="3" cols="50">{{ $existing_entry['workout_notes'] ?? '' }}</textarea>
 
             @error('workout_notes')
             <p class="error">{{$message}}</p>
@@ -61,7 +61,7 @@ $max_date = $new_past_entry ? date('Y-m-d', strtotime('-1 day')) : date("Y-m-d")
         <div class="field field--cheat-meals">
             <label for="cheat_meals">Cheat meals (comma separated)</label>
 
-            <textarea name="cheat_meals" id="cheat_meals" rows="3" cols="50">{{ $existing_entry? $existing_entry['cheat_meals'] : '' }}</textarea>
+            <textarea name="cheat_meals" id="cheat_meals" rows="3" cols="50">{{ $existing_entry['cheat_meals'] ?? '' }}</textarea>
 
             @error('cheat_meals')
             <p class="error">{{$message}}</p>
@@ -71,7 +71,7 @@ $max_date = $new_past_entry ? date('Y-m-d', strtotime('-1 day')) : date("Y-m-d")
         <div class="field field--pages-read">
             <label for="pages_read">Pages read</label>
 
-            <input type="number" step="1" min="0" max="1000" name="pages_read" id="pages_read" value="{{ $existing_entry? $existing_entry['pages_read'] : 0 }}"/>
+            <input type="number" step="1" min="0" max="1000" name="pages_read" id="pages_read" value="{{ $existing_entry['pages_read'] ?? 0 }}"/>
 
             @error('pages_read')
             <p class="err">{{$message}}</p>
@@ -81,7 +81,7 @@ $max_date = $new_past_entry ? date('Y-m-d', strtotime('-1 day')) : date("Y-m-d")
         <div class="field field--general-notes">
             <label for="general_notes">General notes</label>
 
-            <textarea name="general_notes" id="general_notes" rows="3" cols="50">{{ $existing_entry? $existing_entry['general_notes'] : '' }}</textarea>
+            <textarea name="general_notes" id="general_notes" rows="3" cols="50">{{ $existing_entry['general_notes'] ?? '' }}</textarea>
 
             @error('general_notes')
             <p class="error">{{$message}}</p>
