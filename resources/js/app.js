f24ece6bf928d220fe29ae7a82ae49735aacd752
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-09-24 22:34:52 +0100

    expand js to past entries/edit entries

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,6 @@ import './bootstrap';
 function handleCheckboxLogic(checkboxSet) {
     checkboxSet.forEach(element => {
         element.addEventListener('click', function(e) {
-            console.log('print');
             clearCheckboxes(checkboxSet);
             for (var i=0; i < element.value; i++) {
                 checkboxSet[i].checked = true;
@@ -21,5 +20,18 @@ function clearCheckboxes(checkboxSet) {
 let waterCountCheckboxes = document.querySelectorAll('.js-todays-entry input[name="water_count[]"]');
 let workoutCheckboxes = document.querySelectorAll('.js-todays-entry input[name="workouts[]"]');
 
+let editWaterCountCheckboxes = document.querySelectorAll('.js-edit-entry input[name="water_count[]"]');
+let editWorkoutCheckboxes = document.querySelectorAll('.js-edit-entry input[name="workouts[]"]');
+
+let pastWaterCountCheckboxes = document.querySelectorAll('.js-past-entry input[name="water_count[]"]');
+let pastWorkoutCheckboxes = document.querySelectorAll('.js-past-entry input[name="workouts[]"]');
+
+
 handleCheckboxLogic(waterCountCheckboxes);
 handleCheckboxLogic(workoutCheckboxes);
+
+handleCheckboxLogic(editWaterCountCheckboxes);
+handleCheckboxLogic(editWorkoutCheckboxes);
+
+handleCheckboxLogic(pastWaterCountCheckboxes);
+handleCheckboxLogic(pastWorkoutCheckboxes);
--- resources/views/edit-entry.blade.php
+++ resources/views/edit-entry.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
 <section class="">
-    @include('partials._new-entry', ['existing_entry' => $log, 'new_past_entry' => null, 'edit_entry' => true])
+    <div class="js-edit-entry">
+        @include('partials._new-entry', ['existing_entry' => $log, 'new_past_entry' => null, 'edit_entry' => true])
+    </div>
 
     <a href="/home" class="">Back to homepage</a>
 </section>
