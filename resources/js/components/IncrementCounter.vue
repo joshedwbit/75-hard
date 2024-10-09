44e8a0f6fe137712c662995f70dffe75bafedea4
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-10-09 16:55:33 +0100

    v-if, dynamic classes, attribute binding, v-on,

--- resources/js/components/IncrementCounter.vue
+++ resources/js/components/IncrementCounter.vue
@@ -6,10 +6,46 @@
   >
     Counter is: {{ counter }}
   </button>
+
+  <div v-if="showDetails">
+    <p :class="{ 'highlight': highlightTitle }" class="my-title">{{ title }}</p>
+    <button @click="toggleHighlight">Higlight title</button>
+    <p>
+        <a :href="url">cr7</a>
+    </p>
+  </div>
+
+  <div>
+    <button @click="toggleDetails">Show Details</button>
+  </div>
 </template>
 
-<script setup>
+<script>
 import { ref } from 'vue'
 
-const counter = ref(0)
+export default {
+    data() {
+        return {
+            counter: ref(0),
+            title: 'hello world',
+            showDetails: true,
+            url: 'https://en.wikipedia.org/wiki/Cristiano_Ronaldo',
+            highlightTitle: false,
+        };
+    },
+    methods: {
+        toggleDetails() {
+            this.showDetails = !this.showDetails;
+        },
+        toggleHighlight() {
+            this.highlightTitle = !this.highlightTitle;
+        }
+    }
+}
 </script>
+
+<style>
+.highlight {
+    background-color: red;
+}
+</style>
